f4269103581702f994a2f29797e0172b5bd16bef
Tabish Shaikh <bonjour.tabish@gmail.com>
2024-01-10 20:01:14 +0530

    feat: add mint functionality

--- src/components/SupaERC20.sol
+++ src/components/SupaERC20.sol
@@ -1,18 +1,50 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.21;
 
-import {ERC20Base} from "@solidstate/token/ERC20/base/ERC20Base.sol";
-import {ERC20BaseInternal} from "@solidstate/token/ERC20/base/ERC20BaseInternal.sol";
-import {ERC20Extended} from "@solidstate/token/ERC20/extended/ERC20Extended.sol";
-import {ERC20Metadata} from "@solidstate/token/ERC20/metadata/ERC20Metadata.sol";
-import {ERC20MetadataInternal} from "@solidstate/token/ERC20/metadata/ERC20MetadataInternal.sol";
-import {ERC20Snapshot} from "@solidstate/token/ERC20/snapshot/ERC20Snapshot.sol";
-import {ERC20SnapshotInternal, ERC20SnapshotStorage} from "@solidstate/token/ERC20/snapshot/ERC20SnapshotInternal.sol";
-import {ERC20Permit} from "@solidstate/token/ERC20/permit/ERC20Permit.sol";
-import {ERC20PermitInternal} from "@solidstate/token/ERC20/permit/ERC20PermitInternal.sol";
+import { ERC20Base } from "@solidstate/token/ERC20/base/ERC20Base.sol";
+import {
+    ERC20BaseInternal
+} from "@solidstate/token/ERC20/base/ERC20BaseInternal.sol";
+import {
+    ERC20BaseStorage
+} from "@solidstate/token/ERC20/base/ERC20BaseStorage.sol";
+import {
+    ERC20Extended
+} from "@solidstate/token/ERC20/extended/ERC20Extended.sol";
+import {
+    ERC20Metadata
+} from "@solidstate/token/ERC20/metadata/ERC20Metadata.sol";
+import {
+    ERC20MetadataInternal
+} from "@solidstate/token/ERC20/metadata/ERC20MetadataInternal.sol";
+import {
+    ERC20Snapshot
+} from "@solidstate/token/ERC20/snapshot/ERC20Snapshot.sol";
+import {
+    ERC20SnapshotInternal,
+    ERC20SnapshotStorage
+} from "@solidstate/token/ERC20/snapshot/ERC20SnapshotInternal.sol";
+import { ERC20Permit } from "@solidstate/token/ERC20/permit/ERC20Permit.sol";
+import {
+    ERC20PermitInternal
+} from "@solidstate/token/ERC20/permit/ERC20PermitInternal.sol";
+import { Ownable } from "@solidstate/access/ownable/Ownable.sol";
 
-/// TODO: ? USDC Revocable Token Standard => would be cool
-contract SupaERC20 is ERC20Base, ERC20Extended, ERC20Metadata, ERC20Permit, ERC20Snapshot {
+// TODO: ? USDC Revocable Token Standard => would be cool
+contract SupaERC20 is
+    ERC20Base,
+    ERC20Extended,
+    ERC20Metadata,
+    ERC20Permit,
+    ERC20Snapshot,
+    Ownable
+{
+    event Minted(address receiver, uint256 amount);
+
+    constructor() {
+        _setOwner(msg.sender);
+    }
+
     function currentSnapshot() public view returns (uint256 snapshotId) {
         snapshotId = ERC20SnapshotStorage.layout().snapshotId;
     }
@@ -21,15 +53,22 @@ contract SupaERC20 is ERC20Base, ERC20Extended, ERC20Metadata, ERC20Permit, ERC2
         newSnapshotId = ERC20SnapshotInternal._snapshot();
     }
 
-    function _setName(string memory newName) internal virtual override(ERC20MetadataInternal, ERC20PermitInternal) {
+    function _setName(
+        string memory newName
+    ) internal virtual override(ERC20MetadataInternal, ERC20PermitInternal) {
         super._setName(newName);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
-        internal
-        virtual
-        override(ERC20BaseInternal, ERC20SnapshotInternal)
-    {
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override(ERC20BaseInternal, ERC20SnapshotInternal) {
         super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function mint(address receiver, uint256 amount) public onlyOwner {
+        ERC20BaseStorage.layout().balances[receiver] += amount;
+        emit Minted(receiver, amount);
     }
 }
